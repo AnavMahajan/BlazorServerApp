ae437311759e2ce5d22a1311de287b9c6f696dc6
Anav Mahajan <anav.mahajan@gmail.com>
2024-05-20 11:17:15 +1000

    first commit

---
+++ Data/BigQueryResult.cs
@@ -0,0 +1,10 @@
+namespace BlazorServerApp.Data
+{
+    public class BigQueryResult
+    {
+        public string word { get; set; }
+        public string word_count { get; set; }
+        public string corpus { get; set; }
+        public string corpus_date { get; set; }
+    }
+}
